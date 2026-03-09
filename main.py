51960f306fa67407ac49fa6c033df89cf498b88c
Amanda Rangel <rang3lamanda@gmail.com>
2026-03-08 20:26:11 -0400

    feat(api): add Item model with name, price, and optional is_offer field

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # main.py
 from fastapi import FastAPI
 from typing import Union
-
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get("/")
 def read_root():
