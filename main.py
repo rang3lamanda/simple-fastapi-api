e4421f07f959e7e77099cbf3f5175f533f60020c
Amanda Rangel <rang3lamanda@gmail.com>
2026-03-08 20:19:52 -0400

    feat(api): add basic FastAPI app with two endpoints

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # main.py
 from fastapi import FastAPI
+from typing import Union
+
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello!"}
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
